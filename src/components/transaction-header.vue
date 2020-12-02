c5cd54d1ac2ef71d7f51202bdfe386ca30e0fe71
Lucas <hello@lucasmoreira.dev>
2020-12-02 11:06:22 -0500

    Basic setup for components based on designs provided

---
+++ src/components/transaction-header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bt-transaction-header">
+  <h1> Transaction Header </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionHeader',
+  props: {
+    total: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/transaction-table.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bt-transaction-table">
+  <h1>Transaction Table</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionTable',
+  props: {
+    msg: String
+  },
+  async created() {
+    try {
+      const response = await this.$axios.get('/transactions/1.json');
+      console.log(response);
+    }
+    catch (err){
+      console.log('error');
+      console.log(err);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
